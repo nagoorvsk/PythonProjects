c9c89e5df3939b7e355c131fac098f2d089c9b0e
Nagoor Shaik <nagoorvsk@gmail.com>
2026-03-05 17:11:33 -0500

    updated basic and fancy indexing

--- numpy-practice/numpy-index-slice.py
+++ numpy-practice/numpy-index-slice.py
@@ -10,31 +10,32 @@ print(arr[3])
 # Printing multiple index at the same time using fancy indexing...
 print(arr[[0,2,3]])
 
+print("Examples - 2D Array")
 # Accessing elements from a 2D array...
 arr = np.array([[1,4,7],
-                [2,6,8],
-                [3,6,9]])
+                [2,5,8],
+                [3,7,9]])
+print(arr)
 
 # We are going to have row index and column index. Both started with zero(0)...
-
-# Single element...
+# Single element... Index is an integer : Basic indexing
 print(arr[0,1])
 
-# Multiple elements...
+# Multiple elements... Index is list : Fancy indexig
 print("Multiple elements:", arr[[0,1],[1,2]])
 
-"""Single row or Single column"""
 # Single row and all columns...
 print(arr[2])                                         
-
 # All rows and single column...
 print(arr[:,2])
 
-"""Multiple rows and multiple columns"""
+print(arr)
+
+# Rows O to 2 (excluding 2)
+print("Slicing example:", arr[0:2,1])
+
 # Printing multiple rows based on index at the same time...
 print(arr[[0,2]])                                       # Multiple rows
 
 # All rows and multiple columns...
 print(arr[:,[0,2]])
-
-""" Basic slicing for single element and Fancy indexing for multiple elements"""
