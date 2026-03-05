ae328366123c2f605e25a7a246e3841c729b0b8b
Nagoor Shaik <nagoorvsk@gmail.com>
2026-03-05 13:13:45 -0500

    added zeros and arange methods

--- numpy-practice/numpy-basics.py
+++ numpy-practice/numpy-basics.py
@@ -1,5 +1,22 @@
 import numpy as np
 
-# Create a ndarray (N-Dimensional Array)
+# Create a ndarray (N-Dimensional Array)...
 arr = np.array([1,2,3,6,7,8])
+print("Printing 1D array:\n ",arr)
+
+arr_2d = np.array([[1,2,3],
+                   [4,5,6],
+                   [7,8,9]
+                   ])
+print("Printing 2D array:\n",arr_2d)
+
+# Initializing ndarray...
+arr = np.zeros(5)
+print(arr)
+
+# Generating an array with evenly spaced values for a provided range...
+arr = np.arange(1,20,2)
+print(arr)
+
+arr = np.linspace(10,20,2)
 print(arr)
