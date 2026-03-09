1ef07d26a42db7a08f3260fbe10ad3e859c14139
Nagoor Shaik <nagoorvsk@gmail.com>
2026-03-08 22:53:50 -0400

    added main function

---
+++ data-analyst-tool-kit/loader.py
@@ -0,0 +1,14 @@
+import csv
+
+def load_data(file_path):
+    rows = []
+
+    with open(file_path, mode="r", newline="") as file:
+        reader = csv.reader(file)   # reader is a iterator. It reads each cell as a string
+        next(reader)    # skip header
+
+        for row in reader:
+            rows.append([float(x) for x in row])        # Convert data to float
+        print(rows)
+
+
---
+++ data-analyst-tool-kit/main.py
@@ -0,0 +1,6 @@
+from loader import load_data
+import numpy as np
+
+file_path = "data/dataset.csv"
+
+data = load_data(file_path)
